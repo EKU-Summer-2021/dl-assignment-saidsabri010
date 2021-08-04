59c97967561163f717411d896662595198b4afed
said <eger3612@gmail.com>
2021-08-04 11:10:06 +0200

    fix : test

--- tests/polynomial.py
+++ tests/polynomial.py
@@ -1,20 +1,25 @@
-# pylint: disable-all
+"""
+test module
+"""
 import unittest
 import numpy as np
 from src import Polynomial
 
 
 class PolynomialTets(unittest.TestCase):
+    """
+    class docstring
+    """
 
     def test_square_of_three_should_be_nince(self):
+        """
+        test method
+        """
         # given
-        p = Polynomial(np.array([1,0,0]))
-        X_VALUE = 3
-        EXPECTED = 9
+        poli = Polynomial(np.array([1, 0, 0]))
+        x_value = 3
+        expected = 9
         # when
-        ACTUAL = p.evaluate(X_VALUE)
+        actual = poli.evaluate(x_value)
         # then
-        self.assertEqual(EXPECTED, ACTUAL)
-
-    # def test_fails(self):
-    #     self.assertEqual(True, False)
+        self.assertEqual(expected, actual)
